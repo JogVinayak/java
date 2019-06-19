6ca2eb633f48769572b96095a82537ef2d2f7792
vinnerboy <jog@live.in>
2019-06-19 22:28:52 +0530

    Initial commit

---
+++ src/main/java/design/patterns/lambda/StrategyPatternDemo.java
@@ -0,0 +1,18 @@
+package design.patterns.lambda;
+
+import java.util.function.Predicate;
+
+public class StrategyPatternDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Strategy pattern from Lambda");
+
+    }
+}
+
+
+class StrategyImplementation{
+
+
+}
