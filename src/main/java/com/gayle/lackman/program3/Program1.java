6dc8975a3210dd405cae1b21ab00fb636de2c55f
vinnerboy <jog@live.in>
2019-06-21 17:17:26 +0530

    Initial commit

--- src/main/java/com/gayle/lackman/program3/Program1.java
+++ src/main/java/com/gayle/lackman/program3/Program1.java
@@ -5,15 +5,27 @@ package com.gayle.lackman.program3;
 public class Program1 {
 
     public static void main(String[] args) {
-        String  str = "Hello ji";
+        String  str = "Hello ji kaise";
+        System.out.println(str);
         str = removeDuplicates(str);
+        System.out.println(str);
 
     }
     public static String removeDuplicates(String str){
         char[] a = str.toCharArray();
+        int tail = 0;
         for(int i=0; i<a.length; i++){
-
+            int j = 0;
+            for(j=0; j<tail; j++){
+                if (a[i] == a[j]) break;
+            }
+            if (j == tail) {
+                a[tail] = a[i];
+                ++tail;
+            }
         }
+
+        a[tail] = 0;
 
         return new String (a);
      }
