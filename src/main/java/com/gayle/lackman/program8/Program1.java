9d2297a1aab4e2323079e47def9f570e682df77e
vinnerboy <jog@live.in>
2019-06-22 11:51:58 +0530

    Initial commit

--- src/main/java/com/gayle/lackman/program8/Program1.java
+++ src/main/java/com/gayle/lackman/program8/Program1.java
@@ -5,4 +5,42 @@ package com.gayle.lackman.program8;
 //only one call to isSubstring (i.e., “waterbottle” is a rotation of “erbottlewat”).
 
 public class Program1 {
+    public static void main(String[] args) {
+
+        String str1 = "hello";
+        String str2 = "ohlel";
+
+        System.out.println( isRotation(str1, str2)? "Yes rotation ": "No its not a rotation" );
+
+    }
+
+    public static boolean isRotation(String str1, String str2){
+        boolean result = false;
+
+        if(str1.length() != str2.length()){
+            return false;
+        }else{
+            for(int i = 0; i<str1.length(); i++){
+                str1 = rotateOnce(str1);
+                System.out.println(str1);
+                if(str2.equals(str1)){
+                    return true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static String rotateOnce(String str){
+        char[] a = str.toCharArray();
+
+        char firstChar = str.charAt(0);
+
+        for(int i=0; i<a.length-1; i++){
+            a[i] = a[i+1];
+        }
+        a[a.length -1 ] = firstChar;
+
+        return new String(a);
+    }
 }
